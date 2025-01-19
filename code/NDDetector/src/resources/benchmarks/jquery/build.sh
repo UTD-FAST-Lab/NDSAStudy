94337add0b1dd73e0f8b6f02d52305dc710e20e3
AnnabellaM <annabellamiao@gmai.com>
2025-01-19 17:50:23 -0600

    update

--- code/NDDetector/src/resources/benchmarks/jquery/build.sh
+++ code/NDDetector/src/resources/benchmarks/jquery/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 CURDIR=$(pwd)
 
-mkdir -p benchmarks/
-cd benchmarks/
+mkdir -p /benchmarks/
+cd /benchmarks/
 git clone https://github.com/jquery/jquery.git
 cd jquery/
 git checkout 3.7.0
 npm run build
 
-cd $CURDIR
 cd $CURDIR
